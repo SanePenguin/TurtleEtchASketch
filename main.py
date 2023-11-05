e1339f29d83cf96d835a669282035cce6cb2f448
René Delion <rene.leon.delion@gmail.com>
2023-11-06 00:33:02 +0100

    Add Basic Controls

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ screen = t.Screen()
 tur = t.Turtle()
 
 screen.listen()
+screen.onkey(key="w", fun=lambda:tur.forward(10))
+screen.onkey(key="a", fun=lambda:tur.left(10))
+screen.onkey(key="s", fun=lambda:tur.forward(-10))
+screen.onkey(key="d", fun=lambda:tur.right(10))
+screen.onkey(key="c", fun=lambda: screen.clearscreen())
 
 screen.exitonclick()
